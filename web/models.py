c0570ab59c44ceaa660595381f60dc98ccd50551
leily <leily.nourbakhsh36@gmail.com>
2020-02-26 08:55:39 -0800

    income class add

--- web/models.py
+++ web/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -8,3 +9,13 @@ class Expense(models.Model):
     date=models.DateTimeField()
     amount = models.BigIntegerField()
     user= models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date,self.amount)
+
+class income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date,self.amount)
