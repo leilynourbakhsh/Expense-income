d1a10b0a58ed9737996ae5249543a84921bc8a64
leilynourbakhsh <leily.nourbakhsh36@gmail.com>
2020-02-28 05:20:38 -0800

    submit/expense/ and submit/income/

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Expense
 from .models import income
+from .models import Token
 # Register your models here.
 
 admin.site.register(Expense)
 admin.site.register(income)
+admin.site.register(Token)
--- web/views.py
+++ web/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from web.models import Expense,income,Token,User
+from json import JSONEncoder
+from datetime import datetime
 
-# Create your views here.
+
+@csrf_exempt
+def submit_income(request):
+    ## TODO: token might be fake, amount might be fake.
+    this_token = request.POST['token']
+    this_user= User.objects.filter(token__token = this_token).get()
+    if 'date'  not in request.POST:
+        date = datetime.now()
+
+    income.objects.create(user=this_user,amount = request.POST['amount'],
+    text = request.POST['text'], date = date)
+    return JsonResponse({
+    'status' :'ok'
+    },encoder=JSONEncoder)
+
+
+
+
+@csrf_exempt
+def submit_expense(request):
+    ## TODO: token might be fake, amount might be fake.
+    this_token = request.POST['token']
+    this_user= User.objects.filter(token__token = this_token).get()
+    if 'date'  not in request.POST:
+        date = datetime.now()
+
+    Expense.objects.create(user=this_user,amount = request.POST['amount'],
+    text = request.POST['text'], date = date)
+    return JsonResponse({
+    'status' :'ok'
+    },encoder=JSONEncoder)
